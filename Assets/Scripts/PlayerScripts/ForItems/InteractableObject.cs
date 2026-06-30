206bf39698ee4016206bd68929ba0b86ee39f849
sererree <159530999+sererree@users.noreply.github.com>
2026-06-30 18:29:28 +0300

    NewScriptsForItems

--- Assets/Scripts/PlayerScripts/ForItems/InteractableObject.cs
+++ Assets/Scripts/PlayerScripts/ForItems/InteractableObject.cs
@@ -2,21 +2,44 @@ using UnityEngine;
 
 public class InteractableObject : MonoBehaviour
 {
-    
-    void Start()
+    // Создаем список типов объектов
+    public enum ObjectType
     {
-        
+        Door,
+        Button //сюда пишешь тип предмета
     }
 
-    
-    void Update()
-    {
-        
-    }
+    [Header("Выберите тип этого предмета:")]
+    public ObjectType typeOfObject;
 
     public void OnInteract()
     {
-        Debug.Log($"Взаимодействие с объектом");
-        //сюда пишешь логику предмета
+        
+        switch (typeOfObject)
+        {
+            case ObjectType.Door:
+                LogicForDoor();
+                break;
+
+            case ObjectType.Button:
+                LogicForButton();
+                break;
+
+           
+
+             //сюда пишешь новый кейс под новый тип предмета и вызываешь логику
+        }
+    }
+
+    // Ниже описываешь функции под каждый тип
+    private void LogicForDoor()
+    {
+        Debug.Log("Логика открытия двери");
+    }
+
+
+    private void LogicForButton()
+    {
+        Debug.Log("Логика нажатия кнопки");
     }
 }
