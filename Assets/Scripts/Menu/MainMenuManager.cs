fa44dabe331c2ee266d776ec4b81a91c1d8270f4
sererree <159530999+sererree@users.noreply.github.com>
2026-07-07 00:47:01 +0300

    Level2Fix

--- Assets/Scripts/Menu/MainMenuManager.cs
+++ Assets/Scripts/Menu/MainMenuManager.cs
@@ -5,7 +5,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("First level");
+        SceneManager.LoadScene("DaniilA");
     }
 
     public void QuitGame()
--- Assets/Scripts/Menu/PauseMenuManager.cs
+++ Assets/Scripts/Menu/PauseMenuManager.cs
@@ -28,6 +28,10 @@ public class PauseMenu : MonoBehaviour
         pauseMenuPanel.SetActive(true);
         Time.timeScale = 0f;
         isPaused = true;
+
+        
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
     }
 
     public void ResumeGame()
@@ -35,8 +39,11 @@ public class PauseMenu : MonoBehaviour
         pauseMenuPanel.SetActive(false);
         Time.timeScale = 1f;
         isPaused = false;
-    }
 
+       
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
     public void MainMenu()
     {
         Time.timeScale = 1f;
