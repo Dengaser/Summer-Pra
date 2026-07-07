21899b8554e3250f8b7fb7ff2bb1268fd4c78427
sererree <159530999+sererree@users.noreply.github.com>
2026-07-07 10:21:40 +0300

    ZombieUpdate

--- Assets/Scripts/PlayerScripts/MagiicSripts/CatsMagic/CatAbilityUnlock.cs
+++ Assets/Scripts/PlayerScripts/MagiicSripts/CatsMagic/CatAbilityUnlock.cs
@@ -1,10 +1,50 @@
 using UnityEngine;
+using System.Collections;
 
 public class CatAbilityUnlock : MonoBehaviour
 {
     [SerializeField] private string playerTag = "Player";
 
+    [Header("Настройки мяуканья")]
+    [SerializeField] private AudioClip[] meowSounds; 
+    [SerializeField] private float minMeowInterval = 5f;
+    [SerializeField] private float maxMeowInterval = 12f;
+
+    public AudioSource audioSource;
+
     private bool isCollected = false;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Start()
+    {
+        StartCoroutine(MeowRoutine());
+    }
+
+    private IEnumerator MeowRoutine()
+    {
+        while (!isCollected)
+        {
+            float randomWait = Random.Range(minMeowInterval, maxMeowInterval);
+            yield return new WaitForSeconds(randomWait);
+
+            
+            if (!isCollected && meowSounds != null && meowSounds.Length > 0)
+            {
+                
+                int randomIndex = Random.Range(0, meowSounds.Length);
+
+                
+                if (meowSounds[randomIndex] != null)
+                {
+                    audioSource.PlayOneShot(meowSounds[randomIndex]);
+                }
+            }
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -12,17 +52,17 @@ public class CatAbilityUnlock : MonoBehaviour
 
         if (other.CompareTag(playerTag))
         {
-            // Ищем скрипт магий напрямую на игроке
+           
             SwitchMagic playerMagic = other.GetComponent<SwitchMagic>();
 
             if (playerMagic != null)
             {
                 isCollected = true;
 
-                // Вызываем метод открытия, который мы только что добавили в SwitchMagic
+                
                 playerMagic.UnlockNextAbility();
 
-                // Уничтожаем кота на сцене
+                
                 Destroy(gameObject);
             }
             else
